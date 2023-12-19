6f4ee75927674c8259379b483bcfbad853bd02e1
MingyuRomeoKim <rlaalsrb0466@naver.com>
2023-12-19 09:42:55 +0900

    StringClass 기능 추가

--- src/StringClass.php
+++ src/StringClass.php
@@ -61,4 +61,94 @@ class StringClass
 
         return $isNullable ? null : $targetString;
     }
+
+    /**
+     * @param string $path
+     * @param bool $isNullable
+     * @return string|null
+     * @description 주어진 경로에서 디렉토리를 string 문자열로 반환.
+     */
+    public function getDirectoryName(string $path, bool $isNullable = false): ?string
+    {
+        if (!$this->isPathString($path)) {
+            return $isNullable ? null : $path;
+        }
+
+        // 경로를 디렉토리와 파일로 분리
+        $info = pathinfo($path);
+
+        // 디렉토리 이름 반환
+        return $info['dirname'];
+    }
+
+    /**
+     * @param string $path
+     * @param bool $isNullable
+     * @return string|null
+     * @description 주어진 경로에서 baseName을 string 문자열로 반환.
+     */
+    public function getBaseName(string $path, bool $isNullable = false): ?string
+    {
+        if (!$this->isPathString($path)) {
+            return $isNullable ? null : $path;
+        }
+
+        // 경로를 디렉토리와 파일로 분리
+        $info = pathinfo($path);
+
+        // 디렉토리 이름 반환
+        return $info['basename'] ?? ($isNullable ? null : $path);
+    }
+
+    /**
+     * @param string $path
+     * @param bool $isNullable
+     * @return string|null
+     * @description 주어진 경로에서 파일 이름을 string 문자열로 반환.
+     */
+    public function getFileName(string $path, bool $isNullable = false): ?string
+    {
+        if (!$this->isPathString($path)) {
+            return $isNullable ? null : $path;
+        }
+
+        // 경로를 디렉토리와 파일로 분리
+        $info = pathinfo($path);
+
+        // 파일 이름 반환
+        return $info['filename'] ?? ($isNullable ? null : $path);
+    }
+
+    /**
+     * @param string $path
+     * @param bool $isNullable
+     * @return string|null
+     * @description 주어진 경로에서 파일 확장자명을 string 문자열로 반환.
+     */
+    public function getFileExtension(string $path, bool $isNullable = false): ?string
+    {
+        if (!$this->isPathString($path)) {
+            return $isNullable ? null : $path;
+        }
+
+        // 경로를 디렉토리와 파일로 분리
+        $info = pathinfo($path);
+
+        // 파일 확장자 반환
+        return $info['extension'] ?? ($isNullable ? null : $path);
+    }
+
+
+    /**
+     * @param string $path
+     * @return bool
+     * @description 주어진 string 문자열이 path 경로와 일치한지 확인하여 true 혹은 false 반환.
+     */
+    public function isPathString(string $path): bool
+    {
+        if (!file_exists($path) || !is_dir($path)) {
+            return false; // 유효하지 않은 경로 또는 문자열인 경우 false 반환
+        }
+        return true;
+    }
 }
